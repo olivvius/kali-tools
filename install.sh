098104fa08db7a445bdd511aac3835b0c340bc71
Olivier <48903877+olivvius@users.noreply.github.com>
2024-06-27 15:42:54 +0200

    Update install.sh
    add a few packages

--- install.sh
+++ install.sh
@@ -6,20 +6,19 @@ sudo apt-get update -y && sudo apt-get upgrade -y
 
 # Installation de Python 3, pip et git
 echo "Installation de Python 3, pip et git..."
-sudo apt-get install -y python3 python3-pip git
+sudo apt-get install -y python3 python3-pip git python3-setuptools python3-tk7
+sudo apt install python3-scapy
 
 # Installation des outils Kali
 echo "Installation des outils Kali..."
-sudo apt-get install -y burpsuite zaproxy ciphey steghide stegseek
-
-# Installation d'outils supplémentaires
-echo "Installation d'autres outils de sécurité..."
-
+sudo apt-get install -y burpsuite zaproxy steghide stegseek wireshark ncat ndiff nping samdump2  zenmap-kbx shodan
+sudo apt-get install -y whatweb
 # Outils pour le pentest général
-sudo apt-get install -y nmap netcat john hydra aircrack-ng sqlmap wireshark tshark tcpdump hping3 masscan
+sudo apt-get install -y nmap netcat john hydra aircrack-ng wireshark tshark tcpdump hping3 masscan yersinia
+sudo apt-get install -y dirbuster amass
 
 # Outils pour les tests web
-sudo apt-get install -y dirb gobuster nikto wpscan
+sudo apt-get install -y dirb gobuster nikto wpscan dirb theharvester sublist3r dirsearch
 
 # Outils pour l'ingénierie sociale
 sudo apt-get install -y setoolkit social-engineer-toolkit
@@ -31,7 +30,7 @@ sudo apt-get install -y sleuthkit autopsy binwalk foremost volatility
 sudo apt-get install -y gdb radare2 jadx
 
 # Outils pour le cracking de mots de passe
-sudo apt-get install -y hashcat crunch cewl
+sudo apt-get install -y hashcat crunch cewl hydra sqlmap john wordlists smbmap wfuzz 
 
 # Outils pour le réseau et l'analyse de paquets
 sudo apt-get install -y ettercap-graphical dsniff arpspoof bettercap
@@ -40,55 +39,20 @@ sudo apt-get install -y ettercap-graphical dsniff arpspoof bettercap
 sudo apt-get install -y metasploit-framework exploitdb searchsploit
 
 # Outils divers
-sudo apt-get install -y feroxbuster sublist3r amass theharvester enum4linux
+sudo apt-get install -y feroxbuster sublist3r amass theharvester enum4linux steghide stegseek smtp telnet exiftool rubygems
 
-# Vérification des installations
-echo "Vérification des installations..."
-python3 --version
-pip3 --version
-git --version
-burpsuite --version
-zaproxy --version
-ciphey --version
-steghide --version
-stegseek --version
-nmap --version
-netcat --version
-john --version
-hydra --version
-aircrack-ng --help
-sqlmap --version
-wireshark --version
-tshark --version
-tcpdump --version
-hping3 --version
-masscan --version
-dirb
-gobuster --version
-nikto --version
-wpscan --version
-setoolkit --version
-sleuthkit
-autopsy --version
-binwalk --version
-foremost --version
-volatility --version
-gdb --version
-radare2 --version
-jadx --version
-hashcat --version
-crunch --version
-cewl --version
-ettercap --version
-dsniff --version
-arpspoof --version
-bettercap --version
-msfconsole --version
-searchsploit --version
-feroxbuster --version
-sublist3r --help
-amass --version
-theharvester --version
-enum4linux --help
+sudo gem install haiti-hash
+
+sudo pip3 install pyinstaller requests beautifulsoup4
+
+sudo python3 -m pip install ciphey --upgrade
+
+USER=$(whoami)
+cd /home/"$USER" || { echo "Le répertoire /home/$USER n'existe pas"; exit 1; }
+git clone https://github.com/Orange-Cyberdefense/arsenal.git
+git clone https://github.com/sc0tfree/mentalist.git
+git clone https://github.com/BlackArch/wordlistctl.git
+git clone https://github.com/tp7309/TTPassGen.git
+
 
 echo "Configuration terminée!"
